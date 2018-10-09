55c068f76c62178b0cbc75cc622bef7e820094d9
Leigh Halliday <leighhalliday@gmail.com>
2018-10-09 10:16:08 -0400

    Updating blog to display affiliate ads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_country
+
   def default_url_options(options = {})
     { protocol: :https }.merge(options)
   end
 
+  def set_country
+    @country = request.env["HTTP_CF_IPCOUNTRY"].presence || '??'
+  end
 end
